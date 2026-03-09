b1c72cf5086a3adaee980fde8c0ed002a4c3c25c
Kevin Bentub Silva <kevinbentubsilva@MacBook-Air-de-Kevin.local>
2026-03-09 14:36:58 +0100

    enfin

--- demo2.php
+++ demo2.php
@@ -1,12 +1,11 @@
 <?php
-    
-    $age = 20;
-    $role = "admin";
-    $motsdepasse = "motdepasse123";
-?>
-    
-    if ($motsdepasse == "motdepasse123"){
-        echo "Mot de passe correct. Accès autorisé.";
-    } else {
-        echo "Mot de passe incorrect. Accès refusé.";
-    }
+
+$age = 20;
+$role = "admin";
+$motsdepasse = "motdepasse123";
+
+if ($motsdepasse == "motdepasse123"){
+    echo "Mot de passe correct. Accès autorisé.";
+} else {
+    echo "Mot de passe incorrect. Accès refusé.";
+}
