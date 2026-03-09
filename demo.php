a014a6ce675a4a6c64637df49a98ab0e9813b731
Kevin Bentub Silva <kevinbentubsilva@MacBook-Air-de-Kevin.local>
2026-03-09 12:32:54 +0100

    finish

--- demo.php
+++ demo.php
@@ -1,1 +1,6 @@
 <?php
+    $prenom = "Maxime";
+    $entreprise = "Tyrolium";
+    $annee = 2026;
+    echo "Bonjour, je m'appelle $prenom et je travaille chez $entreprise. Nous sommes en $annee.";
+?>
