8f18631da3d41a390ee7085f1aa40d9cf1d720f7
Kevin Bentub Silva <kevinbentubsilva@MacBook-Air-de-Kevin.local>
2026-03-09 14:45:22 +0100

    fini

--- demo2.php
+++ demo2.php
@@ -8,4 +8,10 @@ if ($motsdepasse == "motdepasse123"){
     echo "Mot de passe correct. Accès autorisé.";
 } else {
     echo "Mot de passe incorrect. Accès refusé.";
+
+    if ($age < 18) {
+        echo " Vous êtes mineur. Accès restreint.";
+    } else {
+        echo " Vous êtes majeur. Accès complet.";
+    }
 }
