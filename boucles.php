26887845879893aaadb4635f8bce8471b372b9f4
Kevin Bentub Silva <kevinbentubsilva@MacBook-Air-de-Kevin.local>
2026-03-10 11:00:53 +0100

    fini

---
+++ boucles.php
@@ -0,0 +1,17 @@
+<?php
+
+$project = ["Site Vitrine"];
+
+$projects[] = "Intanet";
+$projects[] = "Application Mobile";
+
+for ($i = 0; $i < 3; $i++) {
+    echo $projects[$i] . "<br>";
+}
+
+$j = 0;
+while ($j < 3) {
+    echo $projects[$j] . "<br>";
+    $j++;
+}
+
